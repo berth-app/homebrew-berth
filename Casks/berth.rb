a5ee8c4c463323ccfca15ad57de2b964da389c30
Carlos Infantes <set.is.set@gmail.com>
2026-03-10 12:20:15 +0100

    Update to v0.4.0

--- Casks/berth.rb
+++ Casks/berth.rb
@@ -1,6 +1,6 @@
 cask "berth" do
-  version "0.0.0"
-  sha256 "PLACEHOLDER"
+  version "0.4.0"
+  sha256 "7808b7393547912681c204de8c359ce83e6b3a7828a2a088084b677975125713"
 
   url "https://github.com/berth-app/berth/releases/download/app-v#{version}/Berth_#{version}_universal.dmg"
   name "Berth"
--- Formula/berth-cli.rb
+++ Formula/berth-cli.rb
@@ -1,8 +1,8 @@
 class BerthCli < Formula
   desc "CLI for Berth deployment control plane"
   homepage "https://getberth.dev"
-  url "https://github.com/berth-app/berth/releases/download/app-v0.0.0/berth-cli-darwin-universal.tar.gz"
-  sha256 "PLACEHOLDER"
+  url "https://github.com/berth-app/berth/releases/download/app-v0.4.0/berth-cli-darwin-universal.tar.gz"
+  sha256 "feb4899f6a46411a9ba6876e60658c0d0afc56daf4ea87f0103a2ee74586dc03"
 
   def install
     bin.install "berth-cli-darwin-universal" => "berth"
